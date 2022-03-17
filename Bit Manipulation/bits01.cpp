4ee2b2edf317614f014ca4dfd88a7ed8c3106140
Satpreet Singh <satpreetvirdi007@gmail.com>
2022-03-17 19:00:19 +0530

    Bit setBit clearBit

--- Bit Manipulation/bits01.cpp
+++ Bit Manipulation/bits01.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
 using namespace std;
-
+ 
+//  Get Bit
 
 int getBit(int n,int pos){
 
     return (n&(1<<pos))!=0;
 }
 
+// Set Bit
+
+int setBit(int n, int pos){
+    return (n | (1<<pos));
+}
+
+
+// Clear Bit
+int clearBit(int n, int pos){
+
+    return (n & (~(1<<pos)));
+};
+
 int main (){
 
-    cout<<getBit(100,4);
+    cout<<clearBit(5,2);
     return 0;
 }
---
+++ Bit Manipulation/bits02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Set Bit
+
+int main (){
+    
+
+    return 0;
+}
