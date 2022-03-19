4dab9f02b20fa26feb1d17d58f4088dda79fff9b
Satpreet Singh <satpreetvirdi007@gmail.com>
2022-03-19 16:19:47 +0530

    Power of 2

--- Bit Manipulation/bits02.cpp
+++ Bit Manipulation/bits02.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-// Set Bit
+// Check if a given number is power of 2 or not 
+// Catch : 
+// if n = 6 = (0110)
+// then n-1 =5 = (0101) , Flipping the rightmost 1 of n and all right side bits gives us n-1
+
+int powerof2(int n){
+    return (n && !(n & (n-1)));
+
+}
+
+
 
 int main (){
     
-
+    cout<<powerof2(32);
     return 0;
 }
