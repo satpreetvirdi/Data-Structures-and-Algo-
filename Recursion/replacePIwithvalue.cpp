8bb037413c9e8cb3c7bf4a33c0b6439c72c53e9e
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-04-23 01:54:04 +0530

    Replace Pi with 3.14

---
+++ Recursion/replacePIwithvalue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// Replace Pi with 3.14
+void replacePi(string s){
+    if(s.length()== 0){
+            return;
+    }
+    if(s[0]== 'p' && s[1] =='i'){
+        cout<<"3.14";
+        replacePi(s.substr(2));
+    }else{
+        cout<<s[0];
+        replacePi(s.substr(1));
+    }
+    
+};
+
+int main(){
+    replacePi("pipppip134pppiipipi");
+    
+}
