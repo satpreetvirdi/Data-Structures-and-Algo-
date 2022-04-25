101f672cb93aa64d4626a58106b2239d30565035
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-04-26 01:51:22 +0530

    Permutation of String

---
+++ Recursion/permutationOfString.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void permutations(string s, string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0 ;i<s.length();++i){
+        char ch = s[i];
+        string ros = s.substr(0,i)+ s.substr(i+1);
+        permutations(ros,ans+ch);
+    }
+}
+
+int main(){
+    permutations("ABC" , " ");
+    return 0;
+}
