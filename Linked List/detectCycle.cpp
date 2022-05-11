c2ca24cc63a893c00f8ee2f792ce868eb96b454b
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-05-11 07:26:20 +0530

    Detect Cycle

---
+++ Linked List/detectCycle.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Insert a Node at tail(end)
+
+void insertAtTail(node *&head, int val)
+{
+
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+bool detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+        {
+
+            return true;
+        }
+    }
+    return false;
+};
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    cout<<detectCycle(head);
+
+    return 0;
+};
