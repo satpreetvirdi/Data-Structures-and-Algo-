f6f1b1a4de52bdc365583dc9b9cd3086817cdc97
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-06-13 01:24:20 +0530

    Equal Strings

--- Codechef/CodechefJuneLong/equalStrings.cpp
+++ Codechef/CodechefJuneLong/equalStrings.cpp
@@ -2,16 +2,29 @@
 using namespace std;
 
 int main(){
-    int t;
+    int t; 
     cin>>t;
     while(t--){
-        string a ="" , b = "";
-        int n;
-        for(int i = 0 ; i< n;++i){
-
+      long long  int n,k, co=0;
+      cin>>n;
+        string a,b,c = " ";
+        cin>>a>>b;
+        for(int i=0; i<n;++i){
+            if(a[i] == b[i]) {
+                co+=1;
+            }
+            else{
+                k+=1;
+                c+=b[i];
+            }
         }
-        cin>> a >> b;
-
-
+        sort(c.begin(), c.end());
+        int ans = 0;
+        for(int i = 0; i<c.size()-1;++i){
+            if(c[i]!=c[i+1]){
+                ans+=1;
+            }
+        }
+        cout<<ans<<endl;
     }
 }
