f75a864afe98e901e8fb45ce4dca4cfb75762af5
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-06-25 17:12:20 +0530

    Right view of Binary Tree using recursionL

---
+++ Binary Trees/rightViewRecursion.cpp
@@ -0,0 +1,17 @@
+// Recursive way
+class Solution{
+    public: 
+       vector<int> rightSideView(TreeNode* root){
+       vector<int> res;
+       recursion(root ,0 ,  res);
+       return res;
+   } 
+    void recursion(TreeNode* root , int level , vector<int> &res){
+        if(root == NULL) return ;
+        if(res.size() == level) res.push_back(root->val);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+        
+    }
+    
+};
