e995fba2cb0c41b792a7a8f7d7fcf24d285e87a5
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-05 22:28:22 +0530

    Bridges in Graphs

--- Graphs/bridgesInGraphs.cpp
+++ Graphs/bridgesInGraphs.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(int node,int parent,vector<int>&vis,vector<int>&tin,vector<int>&low,
+int &timer ,vector<int>adj[]){
+    vis[node] =1;
+    tin[node]= low[node] = timer++;
+    for(auto it:adj[node]){
+        if(it == parent ) continue;
+        if(!vis[it]){
+            dfs(it,node,vis ,tin,low, timer,adj);
+            low[node]= min(low[node],low[it]);
+            if(low[it] > tin[node]){
+                cout<<node<< " "<<it<< endl;
+            }else{
+                low[node] = min(low[node], tin[it]);
+            }
+           }
+    }
+}
+
+
 int main(){
     int n,m;
     cin>>n>>m;
@@ -11,7 +30,13 @@ int main(){
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    vector<int> time(n,-1);
+    vector<int> tin(n,-1);
     vector<int> low(n,-1);
     vector<int> vis(n,0);
+    int timer = 0;
+    for(int i=0;i<n;i++){
+        if(!vis[i]){
+            dfs(i,-1,vis,tin,low,timer,adj);
+        }
+    }
 }
