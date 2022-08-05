63e85a80a2c9096d8111dcb9f3ce48eae08c0350
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-06 01:25:31 +0530

    Articulation point in Graph

--- Graphs/articulationPoint.cpp
+++ Graphs/articulationPoint.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(int node , int parent,vector<int>&vis, vector<int>&tin,vector<int>&low,
+int &timer,vector<int>adj[],vector<int>& isArticulation){
+    vis[node] = 1;
+    tin[node] = low[node] = timer++;
+    int child =0;
+    for(auto it : adj[node]){
+        if(it == parent) continue;
+        if(!vis[it]){
+            dfs(it,node,vis,tin,low,timer,adj,isArticulation);
+            low[node] = min(low[node], low[it]);
+            if(low[it] >= tin[node] && parent!= -1){
+                isArticulation[node] =1 ;
+            }
+            child++;
+        }else{
+            low[node] = min(low[node], tin[it]);
+        }
+    }
+    if(parent == -1 && child>1){
+        isArticulation[node] =1;
+    }
+
+}
 int main(){
     int n , m;
     cin>>n>>m;
@@ -14,4 +37,15 @@ int main(){
     vector<int> tin(n,-1);
     vector<int> low(n,-1);
     vector<int> vis(n,0);
-}
+    vector<int> isArticulation(n,0);
+    int timer =0;
+    for(int i=0;i<n;i++){
+        if(!vis[i]){
+            dfs(i,-1,vis,tin,low,timer,adj,isArticulation);
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(isArticulation[i] == 1) cout << i <<endl;
+    }
+    return 0;
+    }
