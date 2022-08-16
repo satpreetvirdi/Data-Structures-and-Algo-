1eec7e3c4618756306f3e08a7f1a55615adc9836
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-16 23:32:45 +0530

    35. Search Insert Position

---
+++ Array/searchInsertPosition.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0, j = nums.size()-1;
+        while(i <= j)
+        {
+            int m = (i + j) / 2;
+            if(nums[m] == target)
+                return m;
+            if(target < nums[m])
+                j = m-1; 
+            else
+                i = m + 1;
+        }
+        return i;
+    }
+};
