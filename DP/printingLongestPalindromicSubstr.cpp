8eff97a58a9b2f9506497dc7139bcd692d62627f
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-22 18:02:15 +0530

    5. Longest Palindromic Substring

---
+++ DP/printingLongestPalindromicSubstr.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string findPalindrome(string s, int left, int right)
+{
+    int n = s.size();
+    int l = left;
+    int r = right;
+    while (left>=0 && right<=n-1 && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+    return s.substr(left+1, right-left-1);
+}
+
+
+// This is the common solution.
+// Actuatlly it's faster than DP solution under Leetcode's test
+// the below optimized DP solution need 700ms+, this needs around 250ms.
+string longestPalindrome(string s) {
+    int n = s.size();
+    if (n<=1) return s;
+
+    string longest;
+    
+    string str;
+    for (int i=0; i<n-1; i++) {
+        str = findPalindrome(s, i, i);
+        if (str.size() > longest.size()){
+            longest = str;
+        } 
+        str = findPalindrome(s, i, i+1);
+        if (str.size() > longest.size()){
+            longest = str;
+        } 
+    }
+
+    return longest; 
+}
+
+};
