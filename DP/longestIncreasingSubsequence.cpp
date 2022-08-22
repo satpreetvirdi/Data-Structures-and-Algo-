0c4914ddb632f06b85e14ac29a4e58bf6e4b869b
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-22 19:17:57 +0530

    Memoization + Tabulations CPP

--- DP/longestIncreasingSubsequence.cpp
+++ DP/longestIncreasingSubsequence.cpp
@@ -16,3 +16,27 @@ public:
         return f(0,-1,nums,dp);
     }
 };
+
+
+// Tabulation
+int longestIncreasingSubsequence(int arr[], int n){
+    
+    vector<int> dp(n,1);
+    
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i]){
+                dp[i] = max(dp[i], 1 + dp[prev_index]);
+            }
+        }
+    }
+    
+    int ans = -1;
+    
+    for(int i=0; i<=n-1; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    return ans;
+}
