563948c9b5280b008168b5762e4930aab27fe734
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-28 04:36:47 +0530

    206. Reverse Linked List

---
+++ Linked List/lcReverseLL.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !(head -> next)) {
+            return head;
+        }
+        ListNode* node = reverseList(head -> next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return node;
+    }
+};
