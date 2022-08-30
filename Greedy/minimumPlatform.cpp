36bd08e5dfdeb3a20dbc28cb3a4825f1f188d660
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-08-31 03:22:57 +0530

    Minimum Platform

---
+++ Greedy/minimumPlatform.cpp
@@ -0,0 +1,62 @@
+    //{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        sort(arr,arr+n);
+        sort(dep,dep+n);
+        int ptr1=0,ptr2=0;
+        int ans =0;
+        int platforms=0;
+        while(ptr1<n && ptr2<n){
+            if (arr[ptr1]<=dep[ptr2]){
+                ans++;
+                ptr1++;
+            }
+            else{
+                ptr2++;
+                ans--;
+            }
+            platforms=max(ans,platforms);
+        }
+        
+        // ans+=n-ptr1;
+        // platforms=max(ans,platforms);
+        return platforms;
+    	 
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
