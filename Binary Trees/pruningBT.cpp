4a029efd76a052d406aa10e83670224ccbd56cc2
satpreetvirdi <satpreetvirdi701@gmail.com>
2022-09-06 23:07:31 +0530

    814. Binary Tree Pruning

---
+++ Binary Trees/pruningBT.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if(root->val == 0 && root->left==NULL && root->right==NULL) return NULL;
+
+        return root;
+    }
+};
